6468c8bd022780934da5a7e29510a3c860e764f4
HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>
2023-03-05 12:06:16 +0100

    auto advance

--- src/actors/player.c
+++ src/actors/player.c
@@ -90,11 +90,12 @@ void next_sfx() {
 }
 
 u8 show_message(s16 stickFlameTimer, u8 calledSFX, u32 sfxId) {
+    static u32 prevSfxId[6] = {1,2,3,4,5,6};
     Draw_Lock();
     Draw_ClearFramebuffer();
     if (calledSFX) {
         Draw_DrawFormattedString(20, 70, COLOR_TITLE, "No crash");
-        Draw_DrawFormattedString(20, 90, COLOR_WHITE, "stickFlameTimer: %08X", stickFlameTimer);
+        Draw_DrawFormattedString(20, 90, COLOR_WHITE, "stickFlameTimer: 0x%04X (%01d)", stickFlameTimer, stickFlameTimer);
         Draw_DrawFormattedString(20, 110, COLOR_WHITE, "sfxId: %08X", sfxId);
     } else {
         Draw_DrawFormattedString(20, 70, COLOR_WHITE, "Crashy function not called");
@@ -108,15 +109,36 @@ u8 show_message(s16 stickFlameTimer, u8 calledSFX, u32 sfxId) {
     Draw_Unlock();
 
     while(1) {
-        u32 pressed = Input_WaitWithTimeout(1000);
+        u32 timeout;
+        if (sfxId != prevSfxId[0]) {
+            timeout = 1000;
+        } else if (sfxId != prevSfxId[1]) {
+            timeout = 1000;
+        } else if (sfxId != prevSfxId[2]) {
+            timeout = 500;
+        } else if (sfxId != prevSfxId[3]) {
+            timeout = 500;
+        } else if (sfxId != prevSfxId[4]) {
+            timeout = 500;
+        } else if (sfxId != prevSfxId[5]) {
+            timeout = 300;
+        } else {
+            timeout = 300;
+        }
+        prevSfxId[5] = prevSfxId[4];
+        prevSfxId[4] = prevSfxId[3];
+        prevSfxId[3] = prevSfxId[2];
+        prevSfxId[2] = prevSfxId[1];
+        prevSfxId[1] = prevSfxId[0];
+        prevSfxId[0] = sfxId;
+
+        u32 pressed = Input_WaitWithTimeout(timeout);
 
         if(pressed & BUTTON_R1) {
             return RESET_LOOP;
         }
 
-        if(pressed & (BUTTON_A | BUTTON_B)) {
-            return STOP_AND_PLAY_SFX;
-        }
+        return STOP_AND_PLAY_SFX;
     }
 
     return 0;
